104047e7b05f6e3711b7295cca6d5d15d609f5fc
wuzhao <wuzhao@sqbj.com>
2019-06-12 17:40:18 +0800

    修改代码

--- lehome-queue/src/main/java/cn/lehome/dispatcher/queue/listener/house/HouseAddressUpdateListener.java
+++ lehome-queue/src/main/java/cn/lehome/dispatcher/queue/listener/house/HouseAddressUpdateListener.java
@@ -5,6 +5,7 @@ import cn.lehome.dispatcher.queue.listener.AbstractJobListener;
 import cn.lehome.dispatcher.queue.service.house.AddressChangeService;
 import cn.lehome.dispatcher.queue.service.house.AddressChangeServiceHandler;
 import cn.lehome.framework.base.api.core.event.IEventMessage;
+import cn.lehome.framework.base.api.core.event.SimpleEventMessage;
 import org.springframework.beans.factory.annotation.Autowired;
 
 /**
@@ -19,11 +20,12 @@ public class HouseAddressUpdateListener extends AbstractJobListener {
 
     @Override
     public void execute(IEventMessage eventMessage) throws Exception {
-        if (!(eventMessage instanceof AddressNameChangeEvent)) {
+        if (!(eventMessage instanceof SimpleEventMessage)) {
             logger.error("消息类型不对");
             return;
         }
-        AddressNameChangeEvent addressNameChangeEvent = (AddressNameChangeEvent) eventMessage;
+        SimpleEventMessage<AddressNameChangeEvent> simpleEventMessage = (SimpleEventMessage) eventMessage;
+        AddressNameChangeEvent addressNameChangeEvent = simpleEventMessage.getData();
         AddressChangeService addressChangeService = addressChangeServiceHandler.getAddressChangeService(addressNameChangeEvent.getExtendType());
         addressChangeService.changeName(addressNameChangeEvent.getId());
     }
