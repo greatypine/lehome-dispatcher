dde88c48d6cd11698ff554c1b4a373390156d777
wuzhao <wuzhao@sqbj.com>
2019-11-29 11:12:18 +0800

    修改代码

--- lehome-quartz/src/main/java/cn/lehome/dispatcher/quartz/service/invoke/device/EntranceStatusJobServiceImpl.java
+++ lehome-quartz/src/main/java/cn/lehome/dispatcher/quartz/service/invoke/device/EntranceStatusJobServiceImpl.java
@@ -1,14 +1,34 @@
 package cn.lehome.dispatcher.quartz.service.invoke.device;
 
+import cn.lehome.base.api.acs.bean.region.Region;
+import cn.lehome.base.api.acs.service.region.RegionApiService;
+import cn.lehome.base.api.common.component.jms.EventBusComponent;
+import cn.lehome.base.api.common.constant.EventConstants;
 import cn.lehome.base.api.iot.common.bean.entrance.EntranceDevice;
 import cn.lehome.base.api.iot.common.bean.entrance.QEntranceDevice;
 import cn.lehome.base.api.iot.common.service.entrance.EntranceDeviceApiService;
+import cn.lehome.base.api.workorder.bean.event.Event;
+import cn.lehome.base.pro.api.bean.address.AddressBaseInfo;
+import cn.lehome.base.pro.api.bean.area.AreaInfo;
+import cn.lehome.base.pro.api.bean.house.AddressBean;
+import cn.lehome.base.pro.api.bean.regions.ControlRegionsPositionRelationship;
+import cn.lehome.base.pro.api.bean.regions.QControlRegionsPositionRelationship;
+import cn.lehome.base.pro.api.service.address.AddressBaseApiService;
+import cn.lehome.base.pro.api.service.area.AreaInfoApiService;
+import cn.lehome.base.pro.api.service.regions.ControlRegionsApiService;
+import cn.lehome.base.pro.api.service.regions.ControlRegionsPositionRelationshipApiService;
+import cn.lehome.bean.pro.enums.address.ExtendType;
+import cn.lehome.bean.workorder.enums.event.EventType;
+import cn.lehome.bean.workorder.enums.event.PositionType;
+import cn.lehome.bean.workorder.enums.event.TargetType;
 import cn.lehome.dispatcher.quartz.service.AbstractInvokeServiceImpl;
+import cn.lehome.framework.base.api.core.event.SimpleEventMessage;
 import cn.lehome.framework.base.api.core.request.ApiRequest;
 import cn.lehome.framework.base.api.core.request.ApiRequestPage;
 import cn.lehome.framework.base.api.core.response.ApiResponse;
 import cn.lehome.iot.bean.common.enums.entrance.EntranceDeviceType;
 import cn.lehome.iot.bean.common.enums.gateway.OnlineStatus;
+import com.alibaba.fastjson.JSON;
 import com.google.common.collect.Lists;
 import org.apache.commons.lang3.time.DateUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -30,9 +50,29 @@ public class EntranceStatusJobServiceImpl extends AbstractInvokeServiceImpl {
     private EntranceDeviceApiService entranceDeviceApiService;
 
     @Autowired
+    private RegionApiService regionApiService;
+
+    @Autowired
+    private ControlRegionsApiService controlRegionsApiService;
+
+    @Autowired
+    private ControlRegionsPositionRelationshipApiService controlRegionsPositionRelationshipApiService;
+
+    @Autowired
+    private AddressBaseApiService addressBaseApiService;
+
+    @Autowired
+    private AreaInfoApiService proAreaInfoApiService;
+
+    @Autowired
     private StringRedisTemplate stringRedisTemplate;
 
     private static int ONLINE_INTERVAL = -2;
+
+    private static long offlineTime = 2 * 60 * 60 * 1000l;
+
+    @Autowired
+    private EventBusComponent eventBusComponent;
 
     @Override
     public void doInvoke(Map<String, String> params) {
@@ -52,15 +92,68 @@ public class EntranceStatusJobServiceImpl extends AbstractInvokeServiceImpl {
             List<EntranceDevice> updateList = Lists.newArrayList();
 
             for (EntranceDevice entranceDevice : entranceDeviceList) {
-                if (entranceDevice.getDeviceType().equals(EntranceDeviceType.NFC_2G)) {
-                    String key = String.format("%s_%s", "base", entranceDevice.getDeviceUuid());
-                    if (!stringRedisTemplate.hasKey(key)) {
-                        entranceDevice.setOnlineStatus(OnlineStatus.OFFLINE);
+                if (entranceDevice.getOnlineStatus().equals(OnlineStatus.ONLINE)) {
+                    if (entranceDevice.getDeviceType().equals(EntranceDeviceType.NFC_2G)) {
+                        String key = String.format("%s_%s", "base", entranceDevice.getDeviceUuid());
+                        if (!stringRedisTemplate.hasKey(key)) {
+                            entranceDevice.setOnlineStatus(OnlineStatus.OFFLINE);
+                            updateList.add(entranceDevice);
+                        }
+                    } else {
                         updateList.add(entranceDevice);
                     }
                 } else {
-                    updateList.add(entranceDevice);
+                    if (System.currentTimeMillis() - entranceDevice.getUpdatedTime().getTime() >= offlineTime) {
+                        Region region = regionApiService.findByDeviceUuid(entranceDevice.getDeviceUuid());
+                        if (region != null && !region.getDisabled())  {
+                            List<ControlRegionsPositionRelationship> regionsPositionRelationships = controlRegionsPositionRelationshipApiService.findAll(ApiRequest.newInstance().filterEqual(QControlRegionsPositionRelationship.controlRegionId, region.getTraceId()));
+                            if (!CollectionUtils.isEmpty(regionsPositionRelationships)) {
+                                ControlRegionsPositionRelationship relationship = regionsPositionRelationships.get(0);
+                                AreaInfo areaInfo = proAreaInfoApiService.findOne(relationship.getCommunityId().intValue());
+                                AddressBaseInfo addressBaseInfo = null;
+                                PositionType positionType = PositionType.UNIT;
+                                if (relationship.getUnitId() != null) {
+                                    addressBaseInfo = addressBaseApiService.findByExtendId(ExtendType.UNIT, relationship.getUnitId());
+                                } else {
+                                    if (relationship.getFloorId() != null) {
+                                        addressBaseInfo = addressBaseApiService.findByExtendId(ExtendType.BUILDING, relationship.getFloorId());
+                                        positionType = PositionType.BUILDING;
+                                    } else {
+                                        if (relationship.getProjectId() != null) {
+                                            addressBaseInfo = addressBaseApiService.findByExtendId(ExtendType.PROJECT, relationship.getProjectId());
+                                            positionType = PositionType.PROJECT;
+                                        }
+                                    }
+                                }
+                                if (areaInfo != null && addressBaseInfo != null) {
+                                    AddressBean addressBean = JSON.parseObject(addressBaseInfo.getAddress(), AddressBean.class);
+                                    String positionStr = areaInfo.getAreaName();
+                                    if (positionType == PositionType.UNIT) {
+                                        positionStr = positionStr + String.format("%s-%s-%s", addressBean.getProjectName(), addressBean.getBuildingNumber(), addressBean.getUnitNumber());
+                                    } else if (positionType == PositionType.BUILDING) {
+                                        positionStr = positionStr + String.format("%s-%s", addressBean.getProjectName(), addressBean.getBuildingNumber());
+                                    } else if (positionType == PositionType.PROJECT) {
+                                        positionStr = positionStr + addressBean.getProjectName();
+                                    }
+                                    String content = String.format("位于%s的门禁设备(%s)长期掉线", positionStr, entranceDevice.getDeviceUuid());
+                                    Event event = new Event();
+                                    event.setEventTime(new Date());
+                                    event.setContent(content);
+                                    event.setTargetType(TargetType.ENTRANCE_DEVICE);
+                                    event.setTargetId(entranceDevice.getId().toString());
+                                    event.setAreaId(areaInfo.getId());
+                                    event.setPositionId(addressBaseInfo.getExtendId());
+                                    event.setPositionType(positionType);
+                                    event.setTenantId(areaInfo.getUniqueCode());
+                                    event.setType(EventType.ALARM);
+                                    logger.error("发送设备长期掉线告警, deviceUUid = {}", entranceDevice.getDeviceUuid());
+                                    eventBusComponent.sendEventMessage(new SimpleEventMessage<>(EventConstants.EVENT_MESSAGE_EVENT, event));
+                                }
+                            }
+                        }
+                    }
                 }
+
             }
             
             if (!CollectionUtils.isEmpty(updateList)) {
